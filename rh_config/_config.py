76852f2fb502cd74d078f8e20324f6159865b5c8
Lee Kamentsky <lee.kamentsky@gmail.com>
2016-05-19 11:33:00 -0400

    Added get_entry to make it easier to use defaults

--- rh_config/_config.py
+++ rh_config/_config.py
@@ -9,4 +9,26 @@ if os.path.exists(config_filename):
     config = yaml.load(open(config_filename, "r"))
 else:
     config = {}
-all = [config]
+
+def get_entry(defaults, *args):
+    """Get a config entry
+    
+    Get a nested entry from the config file, using a default configuration
+    to supply the default if the config entry is missing.
+    
+    example:
+        defaults = { "foo": { "bar":"baz" } }
+        get_entry(defaults, "foo", "bar")
+        
+    :param defaults: dictionary of defaults.
+    """
+    d = config
+    for arg in args:
+        if arg in d:
+            d = d[arg]
+        else:
+            d = defaults[arg]
+        defaults = defaults[arg]
+    return d
+
+all = [config, get_entry]
