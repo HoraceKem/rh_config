edf4db804afc57ad3587faa1da69a66ee59d4684
Lee Kamentsky <lee.kamentsky@gmail.com>
2016-05-23 08:10:30 -0400

    sry pep8 again

--- rh_config/_config.py
+++ rh_config/_config.py
@@ -10,16 +10,17 @@ if os.path.exists(config_filename):
 else:
     config = {}
 
+
 def get_entry(defaults, *args):
     """Get a config entry
-    
+
     Get a nested entry from the config file, using a default configuration
     to supply the default if the config entry is missing.
-    
+
     example:
         defaults = { "foo": { "bar":"baz" } }
         get_entry(defaults, "foo", "bar")
-        
+
     :param defaults: dictionary of defaults.
     """
     d = config
